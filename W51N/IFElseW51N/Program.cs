b36808060a867337d3e13461631bde44770d88ae
Opilane TTHK <opilane@tthk.ee>
2024-12-19 13:20:14 +0200

    Proov

---
+++ W51N/IFElseW51N/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Threading.Channels;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sisesta üks number");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 0)
+        {
+            Console.WriteLine(number + " on negatiivne arv");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine(number + " on posiitiivne arv");
+        }
+        else
+        {
+            Console.WriteLine("0 pole posiitiivne ega negatiivne arv");
+        }
+    }
+}
