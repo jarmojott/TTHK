a4701eba6ddcda7fa934e4c393fed1ca8e623679
Opilane TTHK <opilane@tthk.ee>
2024-12-20 09:05:06 +0200

    Maakera (2€) ülesanne

---
+++ W51R/AroundEarth/Program.cs
@@ -0,0 +1,15 @@
+﻿
+internal class Program
+{
+    static void Main()
+    {
+        // 25.75mm
+        double d_coinDiameterInKm = 25.75 / 1000000;
+        double r_earthRadiusInKm = 6378;
+
+        double C_EarthInKm = 2 * Math.PI * r_earthRadiusInKm;
+
+        double CoinCount = C_EarthInKm / (d_coinDiameterInKm);
+        Console.WriteLine("2 euroste müntide arv ümber maakera: " + CoinCount);
+    }
+}
