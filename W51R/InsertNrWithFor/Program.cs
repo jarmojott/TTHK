ef6f0d2047a79024ba50bc3f4cd9b8b4c7ad2ad3
Opilane TTHK <opilane@tthk.ee>
2024-12-20 09:38:35 +0200

    Tärnid koos For tsükliga

---
+++ W51R/InsertNrWithFor/Program.cs
@@ -0,0 +1,22 @@
+﻿
+internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Sisesta tärnide arv: ");
+        int kogus = 0;
+        if (int.TryParse(Console.ReadLine(), out kogus))
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (int i = 0; i < kogus; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+    }
+}
