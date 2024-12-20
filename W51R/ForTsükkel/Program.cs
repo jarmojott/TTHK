eca00cd4772622612ef1234e9aee43b400494744
Opilane TTHK <opilane@tthk.ee>
2024-12-20 09:13:35 +0200

    For tsükkel

---
+++ W51R/ForTsükkel/Program.cs
@@ -0,0 +1,11 @@
+﻿    internal class Program
+    {
+        static void Main()
+        {
+            int uhuu = 3;
+            for (int i = 0; i < uhuu; i++)
+            {
+                Console.WriteLine("Tere uhuu kui uhuu on "+uhuu);
+            }
+        }
+    }
