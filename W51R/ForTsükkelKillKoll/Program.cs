c2830406841f4cab198bc4b609b80decd7df5910
Opilane TTHK <opilane@tthk.ee>
2024-12-20 10:14:30 +0200

    KillKoll

---
+++ W51R/ForTsükkelKillKoll/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Sisesta korduste arv: ");
+        int kordus = int.Parse(Console.ReadLine());
+        for (int i = 0; i < kordus; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("kill-koll ");
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nkilladi-koll");
+        }
+    }
+}
