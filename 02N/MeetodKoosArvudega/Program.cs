5fe27c7bc3447812412a9ef6bab34e256e8f265d
Opilane TTHK <opilane@tthk.ee>
2025-01-09 15:04:16 +0200

    Meetodid koos paaris ja paarituga

---
+++ 02N/MeetodKoosArvudega/Program.cs
@@ -0,0 +1,27 @@
+﻿
+internal class Program
+{
+    static void paaris (int arv)
+    {
+        Console.WriteLine(arv+ " on paaris arv.");
+    }
+
+    static void paaritu(int arv)
+    {
+        Console.WriteLine(arv + " on paaritu arv.");
+    }
+
+    static void Main()
+    {
+        Console.Write("Sisesta arv: ");
+        int arv = int.Parse(Console.ReadLine());
+
+        if (int.IsEvenInteger(arv)){
+            paaris(arv);
+        }
+        else
+        {
+            paaritu(arv);
+        }
+    }
+}
