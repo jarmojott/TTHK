9ce9093ac23c4d99864c8a7bd8ed9b7ddd0da1cf
Opilane TTHK <opilane@tthk.ee>
2025-01-09 14:51:19 +0200

    Meetodid ehk funktsioonid

---
+++ 02N/MeetodiKutsumine/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Käivitub põhimeetod..");
+        Jalgratas();
+        Console.Write("Sisesta uste arv: ");
+        Buss(int.Parse(Console.ReadLine()));
+    }
+    private static void Jalgratas()
+    {
+        Console.WriteLine("Käivatasid meetodi: Jalgratas.");
+    }
+
+    private static void Buss(int uks)
+    {
+        Console.WriteLine("Käivatasid meetodi: Buss koos "+ uks+ " uksega");
+    }
+}
+
