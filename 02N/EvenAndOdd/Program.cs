a39326fbd864f256f2fa9ad5812cd6b58a355d07
Opilane TTHK <opilane@tthk.ee>
2025-01-09 14:36:12 +0200

    Paaris või paaritu arv

---
+++ 02N/EvenAndOdd/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Sisesta number: ");
+        int arv = int.Parse(Console.ReadLine());
+
+        if (int.IsEvenInteger(arv))
+        {
+            Console.WriteLine("Paaris arv.");
+        } else
+        {
+            Console.WriteLine("Paaritu arv.");
+        }
+
+        if ( arv % 2 ==0)
+        {
+            Console.WriteLine("Paaris arv.");
+        }
+        else
+        {
+            Console.WriteLine("Paaritu arv.");
+        }
+    }
+}
