270945d32940ae43d96ed1c8633f0ff5061bba5b
Opilane TTHK <opilane@tthk.ee>
2025-01-10 10:50:56 +0200

    Kohvik

--- 02R/CoffeePrice/Program.cs
+++ 02R/CoffeePrice/Program.cs
@@ -3,6 +3,32 @@ internal class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tere tulemast!\n");
+        Console.WriteLine("Menüü:");
+        Console.WriteLine("1. Väike tass");
+        Console.WriteLine("2. Keskmine tass");
+        Console.WriteLine("3. Suur tass");
+
+        Console.Write("\nSinu valik: ");
+
+        byte coffeeSize = byte.Parse(Console.ReadLine());
+        double price = 0.25;
+
+        switch (coffeeSize)
+        {
+            case 1:
+                price += 1.5;
+                break;
+            case 2:
+                price += 2.5;
+                break;
+            case 3:
+                price += 3; break;
+            default:
+                price = 0;
+                Console.WriteLine("VIGA!");
+                break;
+        }
+        Console.WriteLine("Hind: {0:f2}", price);
     }
 }
