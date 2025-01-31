7991f7abb286657ab4a8fb37f2328fdb9d8b6c7d
Opilane TTHK <opilane@tthk.ee>
2025-01-31 10:52:17 +0200

    ForEachLoop

---
+++ 05R/ForEach/ForEach/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        String[] myArray = { "H", "e", "l", "l", "o" };
+
+        foreach (var item in myArray)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
+
