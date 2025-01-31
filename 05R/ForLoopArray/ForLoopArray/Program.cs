6a23e75bf3cbb82639674b6a7705f48bc9130fae
Opilane TTHK <opilane@tthk.ee>
2025-01-31 10:44:11 +0200

    ForLoopArray

---
+++ 05R/ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        String[] myArray = { "H", "e", "l", "l", "o" };
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine(myArray[i]);
+        }
+    }
+}
+
