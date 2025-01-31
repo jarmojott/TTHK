eac8fe421b9e2143eb8e2243fe73e9327f827bfd
Opilane TTHK <opilane@tthk.ee>
2025-01-31 11:16:09 +0200

    ForEach loendur

---
+++ 05R/ForEachSorting/ForEachSorting/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("Hello, World!");
+        String[] genders = { "m", "f", "m", "m", "m", "f", "f", "m", "m", "f" };
+        int m = 0, f = 0;
+
+        foreach (var item in genders)
+        {
+            switch (item)
+            {
+                case "m":
+                    m++;
+                    break;
+                case "f":
+                    f++;
+                    break;
+            }
+           // var result = item == "f" ? f++ : m++;
+        }
+        Console.WriteLine("Males: " + m + "\nFemales: " + f);
+    }
+}
+
